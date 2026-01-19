27133b2926ec5982f2582a0ed692dc4b6ae5cb15
Lucas Rocha <lucas.rocha-dev@outlook.com>
2026-01-18 21:48:59 -0300

    Primeiro Commit

---
+++ aeronave/src/test/java/br/com/sonda/aeronave/AeronaveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sonda.aeronave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeronaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
