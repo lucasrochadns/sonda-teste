10d81f70d692417fabf2025f1aa1d0e22f640ac2
Lucas Rocha <lucas.rocha-dev@outlook.com>
2026-01-20 08:51:34 -0300

    correção backend 2

--- aeronave-banckend/src/main/java/br/com/sonda/aeronave/controller/AeronavesExceptionHandler.java
+++ aeronave-banckend/src/main/java/br/com/sonda/aeronave/controller/AeronavesExceptionHandler.java
@@ -1,14 +1,13 @@
-package com.exemplo.aeronaves.api;
+
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
+
 import java.util.Map;
 
 @RestControllerAdvice
@@ -18,22 +17,5 @@ public class AeronavesExceptionHandler {
     public ResponseEntity<Map<String, Object>> notFound(EntityNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(Map.of("erro", ex.getMessage()));
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, Object>> badRequest(MethodArgumentNotValidException exception) {
-        Map<String, String> campos = new HashMap<>();
-        exception.getBindingResult().getFieldErrors()
-                .forEach(x -> campos.put(x.getField(), x.getDefaultMessage()));
-
-        return ResponseEntity.badRequest()
-                .body(Map.of("erro", "Validação falhou", "campos", campos));
-    }
-
-    @ExceptionHandler(org.springframework.http.converter.HttpMessageNotReadableException.class)
-    public ResponseEntity<Map<String, Object>> invalidPayload(HttpMessageNotReadableException exception) {
-        return ResponseEntity.badRequest().body(Map.of(
-                "erro", "Payload inválido. Verifique campos: "
-        ));
     }
 }
